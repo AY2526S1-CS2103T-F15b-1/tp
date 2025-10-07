d9ce4f980fc6c46273c4341780d84ba2f648bdbb
Jayyy25 <184485982+Jayyyyy25@users.noreply.github.com>
2025-10-08 01:05:26 +0800

    Creates class and exception classes for Claim feature

---
+++ src/main/java/seedu/address/model/claim/exceptions/ClaimAmountExceededException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.claim.exceptions;
+
+/**
+ * Signals that the operation will result in a claim amount exceeding the policy limit.
+ */
+public class ClaimAmountExceededException extends RuntimeException {
+}
