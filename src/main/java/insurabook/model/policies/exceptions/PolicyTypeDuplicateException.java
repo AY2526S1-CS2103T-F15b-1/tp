8758874cfba6908059c747f76e1c87945c91b56a
kevin <kevintannjc@gmail.com>
2025-10-08 15:21:21 +0800

    Fix checkstyle line length

--- src/main/java/insurabook/model/policies/exceptions/PolicyTypeDuplicateException.java
+++ src/main/java/insurabook/model/policies/exceptions/PolicyTypeDuplicateException.java
@@ -3,8 +3,8 @@ package insurabook.model.policies.exceptions;
 import insurabook.model.policies.PolicyType;
 
 /**
- * Signals that the operation will result in duplicate Policy Types (Policy Types are considered duplicates if they have the same
- * name).
+ * Signals that the operation will result in duplicate Policy Types (Policy Types are considered duplicates
+ * if they have the same name).
  */
 public class PolicyTypeDuplicateException extends RuntimeException {
     public PolicyTypeDuplicateException(PolicyType policyType) {
