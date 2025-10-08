ef7d7c86a105cd8c82ceb9f961817ddec57839ac
kevin <kevintannjc@gmail.com>
2025-10-08 15:19:08 +0800

    Add javadoc to exception classes

--- src/main/java/insurabook/model/policies/exceptions/PolicyTypeMissingException.java
+++ src/main/java/insurabook/model/policies/exceptions/PolicyTypeMissingException.java
@@ -1,5 +1,8 @@
 package insurabook.model.policies.exceptions;
 
+/**
+ * Signals that the operation is unable to find a matching Policy Type.
+ */
 public class PolicyTypeMissingException extends IllegalArgumentException {
     public PolicyTypeMissingException(String name, int id) {
         super(String.format("No Policy Type found matching %s and %d", name, id));
