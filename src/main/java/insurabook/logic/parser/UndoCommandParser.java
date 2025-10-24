8aad1abaec4d7ee3089efdf18a14b57905d0793f
pinwen <eng.pin.wen@gmail.com>
2025-10-24 12:54:43 +0800

    JavaDoc comments

--- src/main/java/insurabook/logic/parser/UndoCommandParser.java
+++ src/main/java/insurabook/logic/parser/UndoCommandParser.java
@@ -5,8 +5,16 @@ import static insurabook.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import insurabook.logic.commands.UndoCommand;
 import insurabook.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses empty arguments and creates a new UndoCommand object
+ */
 public class UndoCommandParser implements Parser<UndoCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments and checks it is empty
+     * and returns a UndoCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public UndoCommand parse(String args) throws ParseException {
         if (!args.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoCommand.MESSAGE_USAGE));
