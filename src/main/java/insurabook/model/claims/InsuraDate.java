5d61b5106a4c7c47b809c4f69496dbf49c28e17c
kevin <kevintannjc@gmail.com>
2025-10-28 17:17:04 +0800

    Add format method in InsuraDate for UI

--- src/main/java/insurabook/model/claims/InsuraDate.java
+++ src/main/java/insurabook/model/claims/InsuraDate.java
@@ -59,6 +59,32 @@ public class InsuraDate extends Date {
     }
 
     /**
+     * Returns a formatted date string for UI display.
+     */
+    public String toUiString() {
+        String[] dateParts = this.date.split("-");
+        int year = Integer.parseInt(dateParts[0]);
+        int month = Integer.parseInt(dateParts[1]);
+        int day = Integer.parseInt(dateParts[2]);
+        String monthString = switch (month) {
+            case 1 -> "Jan";
+            case 2 -> "Feb";
+            case 3 -> "Mar";
+            case 4 -> "Apr";
+            case 5 -> "May";
+            case 6 -> "Jun";
+            case 7 -> "Jul";
+            case 8 -> "Aug";
+            case 9 -> "Sep";
+            case 10 -> "Oct";
+            case 11 -> "Nov";
+            case 12 -> "Dec";
+            default -> "";
+        };
+        return String.format("%s %d, %d", monthString, day, year);
+    }
+
+    /**
      * Returns the date string.
      */
     @Override
