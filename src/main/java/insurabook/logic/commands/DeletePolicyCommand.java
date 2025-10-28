9ce5544cf62978a0ef62c2e3a00fd6d8585dcd75
Zi Yong <yonglzy1611@gmail.com>
2025-10-28 16:16:28 +0800

    Fix checkstyle warnings

--- src/main/java/insurabook/logic/commands/DeletePolicyCommand.java
+++ src/main/java/insurabook/logic/commands/DeletePolicyCommand.java
@@ -1,7 +1,6 @@
 package insurabook.logic.commands;
 
 import static insurabook.logic.parser.CliSyntax.PREFIX_CLIENT_ID;
-import static insurabook.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static insurabook.logic.parser.CliSyntax.PREFIX_POLICY_ID;
 import static java.util.Objects.requireNonNull;
 
