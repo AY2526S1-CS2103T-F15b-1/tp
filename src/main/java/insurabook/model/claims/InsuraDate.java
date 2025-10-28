894d814dc6d3f07021df1b0f687ffd29fe8ec2d3
kevin <kevintannjc@gmail.com>
2025-10-28 17:49:04 +0800

    Fix indentation issue with switch-case

--- src/main/java/insurabook/model/claims/InsuraDate.java
+++ src/main/java/insurabook/model/claims/InsuraDate.java
@@ -79,19 +79,19 @@ public class InsuraDate extends Date {
         int month = Integer.parseInt(dateParts[1]);
         int day = Integer.parseInt(dateParts[2]);
         String monthString = switch (month) {
-            case 1 -> "Jan";
-            case 2 -> "Feb";
-            case 3 -> "Mar";
-            case 4 -> "Apr";
-            case 5 -> "May";
-            case 6 -> "Jun";
-            case 7 -> "Jul";
-            case 8 -> "Aug";
-            case 9 -> "Sep";
-            case 10 -> "Oct";
-            case 11 -> "Nov";
-            case 12 -> "Dec";
-            default -> "";
+        case 1 -> "Jan";
+        case 2 -> "Feb";
+        case 3 -> "Mar";
+        case 4 -> "Apr";
+        case 5 -> "May";
+        case 6 -> "Jun";
+        case 7 -> "Jul";
+        case 8 -> "Aug";
+        case 9 -> "Sep";
+        case 10 -> "Oct";
+        case 11 -> "Nov";
+        case 12 -> "Dec";
+        default -> "";
         };
         return String.format("%s %d, %d", monthString, day, year);
     }
