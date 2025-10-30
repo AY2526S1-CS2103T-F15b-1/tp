b474257a8ce565dee8b5c31cf935eae56a465428
pinwen <eng.pin.wen@gmail.com>
2025-10-30 17:15:26 +0800

    Phone, email fields for Client
    Add client now requires phone, email fields
    Edit client allows editing phone, email fields

--- src/main/java/insurabook/logic/parser/AddClientCommandParser.java
+++ src/main/java/insurabook/logic/parser/AddClientCommandParser.java
@@ -4,6 +4,8 @@ import static insurabook.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static insurabook.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
 import static insurabook.logic.parser.CliSyntax.PREFIX_CLIENT_ID;
 import static insurabook.logic.parser.CliSyntax.PREFIX_CLIENT_NAME;
+import static insurabook.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static insurabook.logic.parser.CliSyntax.PREFIX_PHONE;
 
 import java.util.stream.Stream;
 
@@ -12,7 +14,9 @@ import insurabook.logic.parser.exceptions.ParseException;
 import insurabook.model.claims.InsuraDate;
 import insurabook.model.client.Client;
 import insurabook.model.client.ClientId;
+import insurabook.model.client.Email;
 import insurabook.model.client.Name;
+import insurabook.model.client.Phone;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -26,19 +30,21 @@ public class AddClientCommandParser implements Parser<AddClientCommand> {
      */
     public AddClientCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_CLIENT_NAME, PREFIX_BIRTHDAY, PREFIX_CLIENT_ID);
+                ArgumentTokenizer.tokenize(args, PREFIX_CLIENT_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_BIRTHDAY, PREFIX_CLIENT_ID);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_CLIENT_NAME, PREFIX_BIRTHDAY, PREFIX_CLIENT_ID)
+        if (!arePrefixesPresent(argMultimap, PREFIX_CLIENT_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_BIRTHDAY, PREFIX_CLIENT_ID)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddClientCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_CLIENT_NAME, PREFIX_BIRTHDAY, PREFIX_CLIENT_ID);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_CLIENT_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_BIRTHDAY, PREFIX_CLIENT_ID);
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_CLIENT_NAME).get());
+        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         InsuraDate birthday = ParserUtil.parseInsuraDate(argMultimap.getValue(PREFIX_BIRTHDAY).get());
         ClientId clientId = ParserUtil.parseClientId(argMultimap.getValue(PREFIX_CLIENT_ID).get());
 
-        Client client = new Client(name, birthday, clientId);
+        Client client = new Client(name, phone, email, birthday, clientId);
 
         return new AddClientCommand(client);
     }
